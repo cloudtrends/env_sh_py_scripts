46d85b9bd378d0454152c85fdcfc857e935d4504
Your Name <you@example.com>
2014-03-27 17:59:44 +0800

    add some minor scripts

---
+++ high_cpu_monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#http://unix.stackexchange.com/questions/66786/bash-script-that-automatically-kills-processes-when-cpu-memory-usage-gets-too-hi
+
+#http://stackoverflow.com/questions/14885598/bash-script-checking-cpu-usage-of-specific-process
+
+
+#monitor gobbs high cpu process , kill and restart it
+
+
+
